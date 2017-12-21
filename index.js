824d9a4d9742ca67d8f100a20babef426e30d6ad
PerpetualWar <perpetualwarr@gmail.com>
2017-12-21 21:31:09 +0100

    added get one and delete /hotel_api routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const { mongoose } = require('./db/mongoose');
+const { ObjectID } = require('mongodb');
 const { Hotel } = require('./models/hotel');
 
 const app = express();
@@ -39,6 +40,39 @@ app.get('/hotel_api', async (req, res) => {
     res.send(doc)
   } catch (e) {
     res.status(400).send(e)
+  }
+});
+
+app.get('/hotel_api/:id', async (req, res) => {
+  console.log(req.params)
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({ message: 'Item with that ID does not exist' });
+  }
+
+  try {
+    const doc = await Hotel.findById(id);
+    res.send(doc)
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.delete('/hotel_api/:id', async (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({ message: 'Item with that ID does not exist' });
+  }
+
+  try {
+    const doc = await Hotel.findByIdAndRemove(id);
+    if (!doc)
+      return res.status(404).send('doesnt exist')
+    res.send(doc);
+  } catch (e) {
+    res.status(400).send(e);
   }
 });
 
